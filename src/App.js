7f76e705555792bf54334764db62b9a8ad172895
Ifeanyi Lucky <56481172+ifeanyilucky@users.noreply.github.com>
2021-02-19 08:16:50 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Index from "./components/Navbar";
 import Watched from "./components/Watched";
 import WatchList from "./components/WatchList";
