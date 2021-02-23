bd41d7f973a6ef2020e4d8e78e172fedf9c9337a
ifeanyilucky <wfguru2017@gmail.com>
2021-02-23 16:03:18 +0100

    random commits

--- src/components/Popular/index.js
+++ src/components/Popular/index.js
@@ -3,18 +3,25 @@ const Popular = () => {
   const [results, setResults] = useState([]);
   const apiKey = process.env.REACT_APP_API_KEY;
   const regularApi = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`;
+  const [loading, setLoading] = useState(true);
 
   fetch(regularApi)
     .then((res) => res.json())
     .then((data) => {
-      if (!data.error) {
-        setResults(results);
-      }
+      setResults(data.results, { loading: false });
     });
 
   return (
     <div className="py-5 mx-auto text-center col-10">
       <h2 className="bubbleboy">Popular Movie</h2>
+      <h3>{process.env.REACT_APP_API_KEY}</h3>
+      <ul>
+        {!loading ? (
+          results.map((movie) => <li>{movie.title}</li>)
+        ) : (
+          <h2>Loading</h2>
+        )}
+      </ul>
     </div>
   );
 };
